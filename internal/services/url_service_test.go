ee08a0cdb9b8a604f55421159e063e7ea9493d8e
OksanaDmitrieva <oxana_dmitrieva@bk.ru>
2025-03-02 13:47:10 +0400

    Added unit test for short URL generation

--- internal/services/url_service_test.go
+++ internal/services/url_service_test.go
@@ -1,1 +1,19 @@
 package services
+
+import (
+	"testing"
+)
+
+// Проверяем, что длина сгенерированного URL составляет 6 символов
+func TestGenerateShortURL(t *testing.T) {
+	url1 := GenerateShortURL("https://google.com")
+	url2 := GenerateShortURL("https://example.com")
+
+	if len(url1) != 6 {
+		t.Errorf("Ожидали длину 6, но получили %d", len(url1))
+	}
+
+	if url1 == url2 {
+		t.Errorf("Ожидали уникальные значения, но получили одинаковые: %s и %s", url1, url2)
+	}
+}
